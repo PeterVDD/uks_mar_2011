ce782a3d6d45ee91d8d8aa8fd927253d986e4437
Peter Van den Daele <peter.vandendaele@gmail.com>
2011-03-16 13:22:37 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultRequestCommand.cs
+++ product/nothinbutdotnetstore/web/core/DefaultRequestCommand.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultRequestCommand : RequestCommand
     {
+        RequestCriteria criteria;
+
+        public DefaultRequestCommand(RequestCriteria criteria)
+        {
+            this.criteria = criteria;
+        }
+
         public bool can_handle(Request request)
         {
-            throw new NotImplementedException();
+            return criteria(request);
         }
 
         public void process(Request request)
